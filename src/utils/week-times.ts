1eafdae1d84c70edfe6a76f297cb1364536152f4
Huann Almeida <huannvictor@gmail.com>
2024-09-14 18:14:26 -0300

    feat: implement form to include goals

---
+++ src/utils/week-times.ts
@@ -0,0 +1,37 @@
+export const weekTimes = [
+  {
+    value: '1',
+    time: '1 × na semana',
+    emoji: '🥱',
+  },
+  {
+    value: '2',
+    time: '2 × na semana',
+    emoji: '🙂',
+  },
+  {
+    value: '3',
+    time: '3 × na semana',
+    emoji: '😎',
+  },
+  {
+    value: '4',
+    time: '4 × na semana',
+    emoji: '😜',
+  },
+  {
+    value: '5',
+    time: '5 × na semana',
+    emoji: '🤨',
+  },
+  {
+    value: '6',
+    time: '6 × na semana',
+    emoji: '🤯',
+  },
+  {
+    value: '7',
+    time: 'Todos os dias da semana',
+    emoji: '🔥',
+  },
+]
