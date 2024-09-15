d38107abe26954fc01aa1b388de108ac0ec7e6d3
Huann Almeida <huannvictor@gmail.com>
2024-09-14 23:17:35 -0300

    feat: configured tanstack query

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
 import { Dialog } from './components/ui/dialog'
 import { CreateGoal } from './components/create-goal'
 import { Summary } from './components/summary'
-// import { EmptyGoal } from './components/empty-goal'
+import { EmptyGoal } from './components/empty-goal'
+import { useQuery } from '@tanstack/react-query'
+import { getSummary } from './http/get-summary'
 
 export function App() {
+  const { data } = useQuery({
+    queryKey: ['summary'],
+    queryFn: getSummary,
+  })
+
   return (
     <Dialog>
-      {/* <EmptyGoal /> */}
-
-      <Summary />
+      {data?.total ? <Summary /> : <EmptyGoal />}
       <CreateGoal />
     </Dialog>
   )
