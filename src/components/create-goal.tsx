3dcf9fa79110782c139efec7e50a662141c259db
Huann Almeida <huannvictor@gmail.com>
2024-09-16 01:02:18 -0300

    feat: create goals

--- src/components/create-goal.tsx
+++ src/components/create-goal.tsx
@@ -16,8 +16,38 @@ import {
 import { X } from 'lucide-react'
 
 import { weekTimes } from '../utils/week-times'
+import { z } from 'zod'
+import { Controller, useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { createGoal } from '../http/create-goal'
+import { useQueryClient } from '@tanstack/react-query'
+
+const createGoalForm = z.object({
+  title: z.string().min(1, 'Informe a atividade que deseja realizar'),
+  desiredWeeklyFrequency: z.coerce.number().min(1).max(7),
+})
+
+type createGoalForm = z.infer<typeof createGoalForm>
 
 export function CreateGoal() {
+  const queryClient = useQueryClient()
+
+  const { register, control, handleSubmit, formState, reset } =
+    useForm<createGoalForm>({
+      resolver: zodResolver(createGoalForm),
+    })
+
+  async function handleCreateGoal(data: createGoalForm) {
+    await createGoal({
+      title: data.title,
+      desiredWeeklyFrequency: data.desiredWeeklyFrequency,
+    })
+    queryClient.invalidateQueries({ queryKey: ['summary'] })
+    queryClient.invalidateQueries({ queryKey: ['pending-goals'] })
+
+    reset()
+  }
+
   return (
     <DialogContent>
       <div className="flex flex-col h-full gap-6">
@@ -36,7 +66,10 @@ export function CreateGoal() {
           </DialogDescription>
         </div>
 
-        <form action="" className="flex flex-col justify-between flex-1">
+        <form
+          onSubmit={handleSubmit(handleCreateGoal)}
+          className="flex flex-col justify-between flex-1"
+        >
           <div className="flex flex-col gap-6">
             <div className="flex flex-col gap-2">
               <Label htmlFor="title">Qual a atividade?</Label>
@@ -44,21 +77,41 @@ export function CreateGoal() {
                 id="title"
                 autoFocus
                 placeholder="Praticar exercícios, meditar, etc..."
+                {...register('title')}
               />
-              <div className="flex flex-col gap-2">
-                <Label>Quantas vezes na semana?</Label>
-                <RadioGroup>
-                  {weekTimes.map(item => (
-                    <RadioGroupItem value={item.value} key={item.value}>
-                      <RadioGroupIndicator />
-                      <span className="font-medium leading-none text-zinc-300">
-                        {item.time}
-                      </span>
-                      <span className="text-lg leading-none">{item.emoji}</span>
-                    </RadioGroupItem>
-                  ))}
-                </RadioGroup>
-              </div>
+              {formState.errors.title && (
+                <p className="text-sm text-red-400">
+                  {formState.errors.title.message}
+                </p>
+              )}
+            </div>
+            <div className="flex flex-col gap-2">
+              <Label>Quantas vezes na semana?</Label>
+              <Controller
+                control={control}
+                name="desiredWeeklyFrequency"
+                defaultValue={1}
+                render={({ field }) => {
+                  return (
+                    <RadioGroup
+                      onValueChange={field.onChange}
+                      value={String(field.value)}
+                    >
+                      {weekTimes.map(item => (
+                        <RadioGroupItem value={item.value} key={item.value}>
+                          <RadioGroupIndicator />
+                          <span className="font-medium leading-none text-zinc-300">
+                            {item.time}
+                          </span>
+                          <span className="text-lg leading-none">
+                            {item.emoji}
+                          </span>
+                        </RadioGroupItem>
+                      ))}
+                    </RadioGroup>
+                  )
+                }}
+              />
             </div>
           </div>
 
@@ -69,7 +122,9 @@ export function CreateGoal() {
                 Fechar
               </Button>
             </DialogClose>
-            <Button className="flex-1">Salvar</Button>
+            <Button type="submit" className="flex-1">
+              Salvar
+            </Button>
           </div>
         </form>
       </div>
